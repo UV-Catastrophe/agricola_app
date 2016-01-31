9c3c9d386e0e21b78dd8f0f0e8fd091ce9bb3fa2
Charlie Camp <ccamp865@gmail.com>
2016-01-31 15:28:07 -0800

    Setup logging

--- src/main/scala/org/charlie_camp/apps/agricola_online/service/Boot.scala
+++ src/main/scala/org/charlie_camp/apps/agricola_online/service/Boot.scala
@@ -1,19 +1,29 @@
 package org.charlie_camp.apps.agricola_online.service
 
 import akka.actor.{ActorSystem, Props}
-import akka.io.IO
+import akka.io.{IO, Tcp}
 import akka.pattern.ask
 import akka.util.Timeout
+import grizzled.slf4j.Logging
 import spray.can.Http
 
+import scala.concurrent.Await
 import scala.concurrent.duration._
 
-object Boot extends App {
+object Boot extends App with Logging {
   implicit val system = ActorSystem("primary")
 
   val service = system.actorOf(Props[MainRoutes], "main-routes-actor")
 
   implicit val timeout = Timeout(5.seconds)
 
-  IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
+  val tcpBound = Await.result(
+    (IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)).mapTo[Tcp.Bound],
+    60.seconds
+  )
+
+  val actualPort = tcpBound.localAddress.getPort
+
+  info(s"Service running.  actualPort=$actualPort")
+  debug("Showing DEBUG logs")
 }
